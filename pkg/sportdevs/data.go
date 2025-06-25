9b56e619aff37980529ae08824a74c32f6d75505
umbralcalc <umbralcalc@gmail.com>
2025-06-25 08:45:09 +0100

    added more data api querying

--- pkg/sportdevs/data.go
+++ pkg/sportdevs/data.go
@@ -24,3 +24,56 @@ type Match struct {
 	AwayTeamName string `json:"away_team_name"`
 	StartTime    string `json:"start_time"`
 }
+
+type MatchStatisticsResponse struct {
+	MatchID    int                  `json:"match_id"`
+	Statistics []MatchStatisticItem `json:"statistics"`
+}
+
+type MatchStatisticItem struct {
+	Type     string `json:"type"`
+	Period   string `json:"period"`
+	Category string `json:"category"`
+	AwayTeam string `json:"away_team"`
+	HomeTeam string `json:"home_team"`
+}
+
+type MatchPlayerStatisticsResponse []MatchPlayerStatistics
+
+type MatchPlayerStatistics struct {
+	MatchID       int `json:"match_id"`
+	TeamID        int `json:"team_id"`
+	PlayerID      int `json:"player_id"`
+	Carries       int `json:"carries"`
+	CleanBreaks   int `json:"clean_breaks"`
+	MetersRun     int `json:"meters_run"`
+	Offloads      int `json:"offloads"`
+	Passes        int `json:"passes"`
+	TacklesMissed int `json:"tackles_missed"`
+	Tackles       int `json:"tackles"`
+	TryAssists    int `json:"try_assists"`
+	TurnoversWon  int `json:"turnovers_won"`
+}
+
+type MatchLineupResponse struct {
+	ID        int  `json:"id"`
+	Confirmed bool `json:"confirmed"`
+	HomeTeam  Team `json:"home_team"`
+	AwayTeam  Team `json:"away_team"`
+}
+
+type Team struct {
+	Players            []Player `json:"players"`
+	PlayerColorNumber  string   `json:"player_color_number"`
+	PlayerColorPrimary string   `json:"player_color_primary"`
+}
+
+type Player struct {
+	PlayerID           int    `json:"player_id"`
+	Substitute         bool   `json:"substitute"`
+	PlayerName         string `json:"player_name"`
+	ShirtNumber        int    `json:"shirt_number"`
+	JerseyNumber       string `json:"jersey_number"`
+	PlayerHashImage    string `json:"player_hash_image"`
+	PlayerStatisticsID int    `json:"player_statistics_id"`
+}
--- pkg/sportdevs/extract.go
+++ pkg/sportdevs/extract.go
@@ -43,3 +43,63 @@ func FetchMatchIncidents(matchID int) (*MatchesIncidentsResponse, error) {
 	}
 	return &arr[0], nil
 }
+
+func FetchMatchStatistics(matchID int) (*MatchStatisticsResponse, error) {
+	client, err := NewClient()
+	if err != nil {
+		return nil, err
+	}
+	url := fmt.Sprintf("https://rugby.sportdevs.com/matches-statistics?match_id=eq.%d", matchID)
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var arr []MatchStatisticsResponse
+	if err := json.NewDecoder(resp.Body).Decode(&arr); err != nil {
+		return nil, fmt.Errorf("invalid match statistics JSON: %w", err)
+	}
+	if len(arr) == 0 {
+		return nil, fmt.Errorf("no match statistics found")
+	}
+	return &arr[0], nil
+}
+
+func FetchMatchPlayersStatistics(matchID int) ([]MatchPlayerStatistics, error) {
+	client, err := NewClient()
+	if err != nil {
+		return nil, err
+	}
+	url := fmt.Sprintf("https://rugby.sportdevs.com/matches-players-statistics?match_id=eq.%d", matchID)
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var arr []MatchPlayerStatistics
+	if err := json.NewDecoder(resp.Body).Decode(&arr); err != nil {
+		return nil, fmt.Errorf("invalid match players statistics JSON: %w", err)
+	}
+	return arr, nil
+}
+
+func FetchMatchLineups(matchID int) (*MatchLineupResponse, error) {
+	client, err := NewClient()
+	if err != nil {
+		return nil, err
+	}
+	url := fmt.Sprintf("https://rugby.sportdevs.com/matches-lineups?match_id=eq.%d", matchID)
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var arr MatchLineupResponse
+	if err := json.NewDecoder(resp.Body).Decode(&arr); err != nil {
+		return nil, fmt.Errorf("invalid match lineup JSON: %w", err)
+	}
+	return &arr, nil
+}
